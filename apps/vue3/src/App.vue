61e83fe6d8a566309ff4b2f2db68e022d951907a
sonofmagic <1324318532@qq.com>
2022-11-03 23:48:31 +0800

    feat: commit codes

---
+++ apps/vue3/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Sheet, SheetCell, VirtualList, useDataSource, vScrollbar } from '@sonofmagic/ui'
+</script>
+
+<template>
+  <div>
+    vue3
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ apps/vue3/tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media'
+  theme: {},
+  variants: {},
+  plugins: []
+}
