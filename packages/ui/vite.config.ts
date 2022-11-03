a91d383458b5a67db0a64f2aede66547939c6978
sonofmagic <1324318532@qq.com>
2022-11-03 23:42:43 +0800

    feat: commit codes

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import vueJsx from '@vitejs/plugin-vue2-jsx'
+import { visualizer } from "rollup-plugin-visualizer";
 
 const config = defineConfig({
   resolve: {
@@ -24,7 +25,7 @@ const config = defineConfig({
     minify: true,
     outDir: 'dist',
     lib: {
-      entry: path.resolve(__dirname, './src/components/exports.ts'),
+      entry: path.resolve(__dirname, './exports.ts'),
       name: 'vue-dom-sheet',
       // the proper extensions will be added
       fileName: 'vue-dom-sheet',
@@ -34,12 +35,12 @@ const config = defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        dir: path.resolve(__dirname, 'dist'),
+        dir: path.resolve(__dirname, '../ui-dist/dist'),
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
-        plugins: [],
+        plugins: [visualizer()],
       },
     },
   },
